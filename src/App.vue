d95760a8a79726f5b7d305892e90440ea2b3608b
jorj-tsere <g.tsereteli.work@gmail.com>
2017-07-04 16:58:13 +0400

    initial

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+  <p ref="myData">{{ data }} <button v-on:click="doSomething">show dataType</button>   </p>
+  <h2 ref="myDataType"> {{ message }} </h2>
+   <h3 ref="myDataTypeConcat"> {{ gg }} </h3>
+   <input type="text" ref="myInput" /> <button v-on:click="showRefs">show refs</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      data:'test example',
+      message:''
+    }
+   },
+   methods:{
+     doSomething: function(){
+      this.message = typeof this.data 
+     },
+     showRefs(){
+      console.log(this.$refs);
+     }
+   },
+   computed:{
+      gg:function(){
+        return this.data +" "+ this.message 
+     }
+   } 
+
+}
+</script>
+
+<style>
+
+</style>
